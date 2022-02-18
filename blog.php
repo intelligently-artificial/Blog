b289ec8ca0379b7a38bf27915c4ef478f307e98d
sparsh_36 <66721570+intelligently-artificial@users.noreply.github.com>
2022-02-18 16:38:28 +0530

    files added

---
+++ blog.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration page</title>
+    <link rel="stylesheet" href="style.css">
+    
+</head>
+<body>
+
+
+
+
+<div class="container">
+<h2>Write your Blog</h2><hr>
+<form action="connect.php" method="post">
+    <textarea name="blog" cols="100" rows="30"></textarea><br><br>
+    
+    <input type="submit"><br><br>
+
+
+   
+   
+    
+
+</form>
+
+</div>
+
+    
+    
+       
+    
+    
+    
+    <script src="index.js"></script>
+</body>
+</html>
+
---
+++ hp.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP BLOG</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body style="background-color : beige";>
+    
+    <div class="container">
+    <h1>YourBlog.com</h1><br><hr>
+        <h1>Homepage</h1><br><hr><br><br><br><br>
+        <h2>New user,click here</h2>
+        <p><a href="http://localhost/proj/reg.php">Register</a></p><br><br><br><br><br><br>
+        <h2>Already a user,click here</h2>
+        <p><a href="http://localhost/proj/index.php">Login</a></p>
+
+
+
+        
+        
+       
+      
+    </div>
+
+    
+    <script src="index.js"></script>
+
+</body>
+</html>
